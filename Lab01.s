0bb249cdd3fda549a8ff8fe91d022638c3dd96cb
KrishKumar969 <kk09969@st.habib.edu.pk>
2026-01-15 11:51:34 +0500

    Task 3 added

---
+++ Lab01.s
@@ -0,0 +1,38 @@
+.text
+.globl main
+main:
+    # Task 3
+
+    # li x1, 5 #a
+    # li x2, 0 #b
+
+    # addi x1,x2,32
+
+    # li x3, 0 #d
+
+    # li x8, 5
+
+    # add x3,x1,x2 # d = a + b
+    # sub x3,x3,x8 # d -5
+
+    # li x4, 0 # a-d
+
+    # sub x4,x1,x3 # a-d
+
+    # li x5, 0 
+
+    # sub x5,x2,x1 #b-a
+
+    # li x6, 0 #e
+
+    # add x6,x4,x5
+    # add x6,x6,x3
+
+    # add x6,x6,x6
+    # add x6,x6,x1
+    # add x6,x6,x2
+    # add x6,x6,x3
+    
+end:
+    j end
+
